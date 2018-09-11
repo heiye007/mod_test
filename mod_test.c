f7a227531020e54ad2b2966ccbfc3be480c4d95a
J.O <jo.o.olsson@gmail.com>
2018-09-11 23:26:53 +0200

    cdev

--- mod_test.c
+++ mod_test.c
@@ -3,12 +3,21 @@
 #include <linux/version.h>
 #include <linux/moduleparam.h>
 #include <linux/fs.h>
+#include <linux/cdev.h>
 
-MODULE_LICENSE("LGPL");
+MODULE_LICENSE("GPL");
 
 static int num = 0;
 static dev_t dev = -1;
 static const char *dev_name = "jool_mod";
+struct cdev *jool_cdev = NULL;
+
+static int jool_open(struct inode *inode, struct file *filp);
+
+struct file_operations jool_fops = {
+	.owner =	THIS_MODULE,
+	.open =		jool_open,
+};
 
 module_param(num, int, S_IRUGO);
 
@@ -18,7 +27,13 @@ static int __init initialize(void)
 		printk(KERN_ALERT "Failed alloc char device \"%s\" region!\n", dev_name);
 		return -1;
 	}
-	printk(KERN_ALERT "Successfully alloced char device \"%s\" region! %d\n", dev_name, dev);
+	printk(KERN_WARNING "Successfully alloced char device \"%s\" region! %d", dev_name, MAJOR(dev));
+
+//	jool_cdev = cdev_alloc();
+//	jool_cdev->ops = jool_fops;
+
+//	cdev.owner = THIS_MODULE;
+
 	return 0;
 }
 
